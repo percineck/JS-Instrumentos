0008379aabb2fda0305f5b4e17c55a7d126ea43f
Luis Eduardo Meneghel Percicoti <86788255+percineck@users.noreply.github.com>
2022-07-29 16:59:29 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,14 +1,46 @@
-function tocaSom (idElementoAudio) {
-    document.querySelector(idElementoAudio).play();
+function tocaSom (seletorAudio) {
+    const elemento = document.querySelector(seletorAudio);
+
+    if(elemento && elemento.localName === 'audio') {
+        elemento.play();
+    }
+    else {
+        console.log('Elemento não encotrado ou seletor inválido');
+    }
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-//let contador = 0;
+//*********** COM FOR ***********
+for (contador=0; contador < listaDeTeclas.length; contador++) {
+
+    const tecla = listaDeTeclas[contador];
+
+    const instrumento = tecla.classList[1];
+
+    const idAudio = `#som_${instrumento}`;
+    
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
+
+    tecla.onkeydown = function (evento) {
+        if(evento.code === "Enter" || evento.code === "Space") {
+            tecla.classList.add('ativa');
+        }
+    }
+
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+    }
+
+}
 
 //---------------------------------------------------------------
+//********** COM WHILE ***********
 
-//COM WHILE
+//let contador = 0;
+
 // while (contador < listaDeTeclas.length) {
 
 //     const tecla = listaDeTeclas[contador];
@@ -28,29 +60,3 @@ const listaDeTeclas = document.querySelectorAll('.tecla');
 // }
 
 //---------------------------------------------------------------
-
-//COM FOR
-for (contador=0; contador < listaDeTeclas.length; contador++) {
-
-    const tecla = listaDeTeclas[contador];
-
-    const instrumento = tecla.classList[1];
-
-    const idAudio = `#som_${instrumento}`;
-    
-    tecla.onclick = function () {
-        tocaSom(idAudio);
-    }
-
-    tecla.onkeydown = function (evento) {
-        if(evento.code === "Enter" || evento.code === "Space") {
-            tecla.classList.add('ativa');
-    
-        }
-    }
-
-    tecla.onkeyup = function () {
-        tecla.classList.remove('ativa');
-    }
-
-}
